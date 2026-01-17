780b9fcc67185a48ae68922212c7bb000356b4ae
yomonom <yomonom@digamma.dev>
2026-01-17 11:39:07 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "2.3.0"
+    id("org.jetbrains.intellij.platform") version "2.10.5"
+}
+
+group = "dev.digamma"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdea("2025.3")
+    }
+}
+
+kotlin {
+    jvmToolchain(25)
+}
